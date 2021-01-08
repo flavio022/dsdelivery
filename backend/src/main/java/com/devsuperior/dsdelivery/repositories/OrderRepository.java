ca4fd86f24ccec03347a039324408bb3d0c0ea83
Flavio <flavio.danilo022@gmail.com>
2021-01-07 22:44:37 -0300

    Adding productController

--- backend/src/main/java/com/devsuperior/dsdelivery/repositories/OrderRepository.java
+++ backend/src/main/java/com/devsuperior/dsdelivery/repositories/OrderRepository.java
@@ -1,5 +1,9 @@
 package com.devsuperior.dsdelivery.repositories;
 
-public class OrderRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsdelivery.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
 }
--- backend/src/main/java/com/devsuperior/dsdelivery/repositories/ProductRepository.java
+++ backend/src/main/java/com/devsuperior/dsdelivery/repositories/ProductRepository.java
@@ -1,5 +1,11 @@
 package com.devsuperior.dsdelivery.repositories;
 
-public class ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devsuperior.dsdelivery.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
 
 }
